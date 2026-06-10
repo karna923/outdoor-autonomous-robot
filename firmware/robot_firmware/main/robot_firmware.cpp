6673f86f4778a0d3fafa0c0a163a91c40ef3a8f4
Pushkar <pushkarna923@gmail.com>
2026-06-10 15:34:28 -0500

    feat: add encoder ISRs and GPIO interrupt setup for ESP32 #1

--- firmware/robot_firmware/main/robot_firmware.cpp
+++ firmware/robot_firmware/main/robot_firmware.cpp
@@ -4,13 +4,34 @@
 #include "pins_esp1.hpp"
 #include "motor_control.hpp"
 #include <cstddef>
-
+#include "driver/gpio.h"
 
 Motor* motor_fl = nullptr;
 Motor* motor_fr = nullptr;
 Motor* motor_ml = nullptr;
 Motor* motor_mr = nullptr;
 
+volatile int32_t enc_count_fl = 0;
+volatile int32_t enc_count_fr = 0;
+volatile int32_t enc_count_ml = 0;
+volatile int32_t enc_count_mr = 0;
+
+
+void IRAM_ATTR isr_enc_fl() {
+    enc_count_fl++;
+}
+  
+void IRAM_ATTR isr_enc_fr() {
+    enc_count_fr++;
+  }
+
+void IRAM_ATTR isr_enc_ml() {
+    enc_count_ml++;
+  }
+
+void IRAM_ATTR isr_enc_mr() {
+    enc_count_mr++;
+  }
 void control_task(void *param) {
   while(1) {
     printf("control loop running\n");
@@ -20,6 +41,51 @@ void control_task(void *param) {
 
 extern "C" void app_main(void) {
   Motor::init_timer();
+
+  gpio_install_isr_service(0);
+
+
+  gpio_config_t enc_config_fl = {
+    .pin_bit_mask = (1ULL << ENC_FL),
+    .mode = GPIO_MODE_INPUT,
+    .pull_up_en = GPIO_PULLUP_DISABLE,
+    .pull_down_en = GPIO_PULLDOWN_DISABLE,
+    .intr_type = GPIO_INTR_ANYEDGE,
+};
+gpio_config(&enc_config_fl);
+
+gpio_config_t enc_config_fr = {
+    .pin_bit_mask = (1ULL << ENC_FR),
+    .mode = GPIO_MODE_INPUT,
+    .pull_up_en = GPIO_PULLUP_DISABLE,
+    .pull_down_en = GPIO_PULLDOWN_DISABLE,
+    .intr_type = GPIO_INTR_ANYEDGE,
+};
+gpio_config(&enc_config_fr);
+
+gpio_config_t enc_config_ml = {
+    .pin_bit_mask = (1ULL << ENC_ML),
+    .mode = GPIO_MODE_INPUT,
+    .pull_up_en = GPIO_PULLUP_DISABLE,
+    .pull_down_en = GPIO_PULLDOWN_DISABLE,
+    .intr_type = GPIO_INTR_ANYEDGE,
+};
+gpio_config(&enc_config_ml);
+
+gpio_config_t enc_config_mr = {
+    .pin_bit_mask = (1ULL << ENC_MR),
+    .mode = GPIO_MODE_INPUT,
+    .pull_up_en = GPIO_PULLUP_DISABLE,
+    .pull_down_en = GPIO_PULLDOWN_DISABLE,
+    .intr_type = GPIO_INTR_ANYEDGE,
+};
+gpio_config(&enc_config_mr);
+
+gpio_isr_handler_add((gpio_num_t)ENC_FL, isr_enc_fl, nullptr);
+gpio_isr_handler_add((gpio_num_t)ENC_FR, isr_enc_fr, nullptr);
+gpio_isr_handler_add((gpio_num_t)ENC_ML, isr_enc_ml, nullptr);
+gpio_isr_handler_add((gpio_num_t)ENC_MR, isr_enc_mr, nullptr);
+
   motor_fl = new Motor(MOTOR_FL_A, MOTOR_FL_B, LEDC_CHANNEL_0, LEDC_CHANNEL_1);
   motor_fr = new Motor(MOTOR_FR_A, MOTOR_FR_B, LEDC_CHANNEL_2, LEDC_CHANNEL_3);
   motor_ml = new Motor(MOTOR_ML_A, MOTOR_ML_B, LEDC_CHANNEL_4, LEDC_CHANNEL_5);
@@ -30,3 +96,4 @@ extern "C" void app_main(void) {
 
   
   }
+
