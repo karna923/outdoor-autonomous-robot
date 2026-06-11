699253225cc1630847b9d7028d2bceafb85b91c6
Pushkar <pushkarna923@gmail.com>
2026-06-11 17:24:42 -0500

    add rplidar launch file for UDP-socat virtual serial bridge

---
+++ ros2_ws/src/outdoor_robot/launch/rplidar.launch.py
@@ -0,0 +1,19 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            name='rplidar_composition',
+            package='rplidar_ros',
+            executable='rplidar_composition',
+            output='screen',
+            parameters=[{
+                'serial_port': '/tmp/ttyLidar',
+                'serial_baudrate': 460800,
+                'frame_id': 'laser',
+                'inverted': False,
+                'angle_compensate': True,
+            }],
+        ),
+    ])
