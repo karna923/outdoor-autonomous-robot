3f554bf1dfae25044e6747f2e2d377cc92848ef6
Pushkar <pushkarna923@gmail.com>
2026-06-11 15:16:57 -0500

    esp32_2: add WiFi station init, UART-UDP lidar bridge tasks

--- firmware/esp32_2/main/robot_firmware.cpp
+++ firmware/esp32_2/main/robot_firmware.cpp
@@ -6,6 +6,20 @@
 #include <cstddef>
 #include "driver/gpio.h"
 
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_netif.h"
+#include "nvs_flash.h"
+#include "freertos/event_groups.h"
+#include "esp_log.h"
+#include "wifi_config.hpp"
+#include <cstring>
+
+#include "lwip/sockets.h"
+#include "netinet/in.h"
+#include "arpa/inet.h"
+#include "driver/uart.h"
+
 Motor* motor_rl = nullptr;
 Motor* motor_rr = nullptr;
 
@@ -13,7 +27,81 @@ Motor* motor_rr = nullptr;
 volatile int32_t enc_count_rl = 0;
 volatile int32_t enc_count_rr = 0;
 
+void udp_to_uart_task(void *param) {
+  uint8_t buf[1024];
 
+  int sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+  struct sockaddr_in dest;
+  dest.sin_family = AF_INET;
+  dest.sin_port = htons(LIDAR_CMD_PORT);
+  dest.sin_addr.s_addr = INADDR_ANY;
+  bind(sock, (struct sockaddr*)&dest, sizeof(dest));
+  while(1) {
+      int len = recvfrom(sock, buf, sizeof(buf), 0, NULL, NULL);
+      uart_write_bytes(UART_NUM_2, buf, len);
+  }
+}
+
+void uart_to_udp_task(void *param) {
+  uint8_t buf[1024];
+
+  int sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+  struct sockaddr_in dest;
+  dest.sin_family = AF_INET;
+  dest.sin_port = htons(LIDAR_BRIDGE_PORT);
+  dest.sin_addr.s_addr = inet_addr(DESKTOP_IP);
+
+  while(1) {
+    int len = uart_read_bytes(UART_NUM_2, buf, sizeof(buf), pdMS_TO_TICKS(10));
+    if (len > 0) {
+      sendto(sock, buf, len, 0, (struct sockaddr*)&dest, sizeof(dest));
+    }
+  }
+}
+
+static EventGroupHandle_t s_wifi_event_group;
+#define WIFI_CONNECTED_BIT BIT0
+
+static void wifi_event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    esp_wifi_connect();
+  } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+    ESP_LOGW("wifi", "Disconnected, retrying...");
+    esp_wifi_connect();
+  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+    ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+    ESP_LOGI("wifi", "Got IP: " IPSTR, IP2STR(&event->ip_info.ip));
+    xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+  }
+}
+
+void wifi_init_sta(void) {
+  s_wifi_event_group = xEventGroupCreate();
+
+  ESP_ERROR_CHECK(nvs_flash_init());
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+  esp_netif_create_default_wifi_sta();
+
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL));
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL, NULL));
+
+  wifi_config_t wifi_config = {};
+  memcpy(wifi_config.sta.ssid, WIFI_SSID, strlen(WIFI_SSID));
+  memcpy(wifi_config.sta.password, WIFI_PASSWORD, strlen(WIFI_PASSWORD));
+
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_start());
+
+  xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT, pdFALSE, pdTRUE, portMAX_DELAY);
+}
 
 void IRAM_ATTR isr_enc_rl(void* arg) {
     enc_count_rl+=1;
@@ -82,10 +170,26 @@ extern "C" void app_main(void) {
   motor_rr->setPID(50.0f, 5.0f, 0.5f);
 
 
+  uart_config_t uart_config = {};
+  uart_config.baud_rate =  460800;
+  uart_config.data_bits = UART_DATA_8_BITS;
+  uart_config.parity = UART_PARITY_DISABLE;
+  uart_config.stop_bits = UART_STOP_BITS_1;
+  uart_config.flow_ctrl = UART_HW_FLOWCTRL_DISABLE;
+
+  uart_param_config(UART_NUM_2, &uart_config);
+  uart_set_pin(UART_NUM_2, RPLIDAR_TX, RPLIDAR_RX, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+  uart_driver_install(UART_NUM_2, 2048, 0, 0, NULL, 0);
+
+  wifi_init_sta();
+
+
   xTaskCreate(control_task, "control_task",
               4096, (void*)"Hello ESP32",
               5, NULL);
 
+  xTaskCreate(uart_to_udp_task, "uart_to_udp", 4096, NULL, 4, NULL);
 
+  xTaskCreate(udp_to_uart_task, "udp_to_uart", 4096, NULL, 4, NULL);
   }
 
